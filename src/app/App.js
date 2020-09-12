c10868f0337597d179dae246aaf7ce10a1ad062a
Astel Thottankara <57724542+astonizer@users.noreply.github.com>
2020-09-12 23:16:27 +0530

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from '../components/Home'
+import Header from '../components/Header'
+import Rater from '../components/Rater'
+import NoMatch from '../components/NoMatch'
 
 function App() {
   return (
-    <div className="container">
-      <Home />
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/:lang' component={Rater} />
+          <Route component={NoMatch} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
