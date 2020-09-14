d2c4223263c7f37f8b038641be51a268897580bc
Astel Thottankara <57724542+astonizer@users.noreply.github.com>
2020-09-14 18:13:57 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
+import data from '../data/data.json'
+import Rater from './rater/Rater'
 
-function Home() {
+const Home = () => {
+    const [frameworks, setFrameworks] = useState([])
+    const handleChange = (e) => {
+        const frames = data[e.target.id]   //Array of framework objects
+        try {
+            let dupFrameworks = frames
+            let maxStars = 0
+            frames.forEach(async frame => {
+                let result = await axios(`https://api.github.com/repos/${frame.user}/${frame.repo}`)
+                let stars = result.data.stargazers_count
+                frame.githubStars = stars
+            })
+            setFrameworks(dupFrameworks)
+        } catch(err) {
+            console.log(err)
+        }
+    }
     return(
         <div className="home">
-            <span>Select your programming language</span>
+            <span>Select your prefered language</span>
             <div className="options">
-                <a href="/javascript" className="link1">Javascript</a>
-                <a href="/python" className="link1">Python</a>
-                <a href="/php" className="link1">PHP</a>
-                <a href="/java" className="link2">Java</a>
-                <a href="/css" className="link2">CSS</a>
-                <a href="/ruby" className="link2">Ruby</a>
+                <button onClick={handleChange} id='javascript'>Javascript</button>
+                <button onClick={handleChange} id='python'>Python</button>
+                <button onClick={handleChange} id='php'>PHP</button>
+                <button onClick={handleChange} id='java'>Java</button>
+                <button onClick={handleChange} id='css'>CSS</button>
+                <button onClick={handleChange} id='ruby'>Ruby</button>
             </div>
+            <Rater frameworks={frameworks} />
         </div>
     );
 }
