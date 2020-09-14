e937222ecb6a84059a78df9345c9b4d514699300
Astel Thottankara <57724542+astonizer@users.noreply.github.com>
2020-09-14 18:15:27 +0530

    Update Icons.js

--- src/components/icons/Icons.js
+++ src/components/icons/Icons.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const Icons = () => {
+const Icons = (props) => {
+    const handleGithub = (e) => {
+        e.preventDefault();
+        props.history.push('https://github.com/astonizer/framework-rater')
+    }
     return (
         <div className='icons'>
             <img src={'https://raw.githubusercontent.com/rdimascio/icons/master/icons/github.svg'} alt="github" width='25' align='right' />
-            <img src={'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/svgs/solid/home.svg'} alt="home" width='25' align='right' />
+            <img onClick={handleGithub} src={'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/svgs/solid/home.svg'} alt="home" width='25' align='right' />
         </div>
     )
 }
